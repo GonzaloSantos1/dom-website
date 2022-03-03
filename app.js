784a6624c8f0ebd3b7bacd995694d026784ca17a
GonzaloSantos1 <gsanore@gmail.com>
2022-03-03 22:05:08 +0100

    minor changes

--- app.js
+++ app.js
@@ -1,16 +1,21 @@
 // SELECTORS
 const header$$ = document.getElementsByTagName('header')[0];
 const navList = ['Live Streams', 'Tournaments', 'About Us', 'Contact'];
-
 // EVENT LISTENERS
 headerNavbar();
 
 // FUNCTIONS
+
+//Header logo + navbar
 function headerNavbar() {
   //Logo div
   const logoDiv = document.createElement('div');
   logoDiv.classList.add('logo-container');
   header$$.appendChild(logoDiv);
+  const logoTitle = document.createElement('h1');
+  logoTitle.classList.add('logo-title');
+  logoDiv.appendChild(logoTitle);
+  logoTitle.textContent = 'DODOM.';
   //Navbar div
   const navbarDiv = document.createElement('div');
   navbarDiv.classList.add('navbar-container');
